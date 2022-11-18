c76167722670778a067a28156759b958617115ab
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-11-18 16:45:21 -0300

    arquivos

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario></Formulario>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+export default function Banner(){
+    return(
+        // JSX
+        <header className='banner'>
+            <img src="/images/banner.png" alt="O banner principal da página Organo"/>
+        </header>
+        )
+
+
+}
